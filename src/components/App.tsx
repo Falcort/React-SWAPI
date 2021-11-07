38658a7cef6bf315646c2e76b923e570ddde81cd
SOUQUET Thibault <contact@thibaultsouquet.fr>
2021-11-07 17:47:48 +0100

    Better menu

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import '../styles/App.css';
 import { Layout, Menu } from 'antd';
-import { Routes, Route, Link } from 'react-router-dom';
+import {
+  Routes, Route, Link, useParams,
+} from 'react-router-dom';
 import GenericTableView from './GenericTableView';
 import GenericObjectView from './GenericObjectView';
 
 const { Header, Sider, Content } = Layout;
 
 function App() {
+  const [currentEndpoint, setCurrentEndpoint] = React.useState<string[]>([]);
+
+  React.useEffect(() => {
+    (async () => {
+      setCurrentEndpoint([window.location.pathname.split('/')[1] || '']);
+    })();
+  }, []);
+
+  const handleClick = (e: any) => {
+    setCurrentEndpoint([e.key]);
+  };
+
   return (
     <Layout>
       <Header
@@ -26,34 +40,35 @@ function App() {
         >
           <Menu
             mode="inline"
-            defaultSelectedKeys={['1']}
+            selectedKeys={currentEndpoint}
+            onClick={handleClick}
           >
-            <Menu.Item key="1">
+            <Menu.Item key="people">
               <Link to="/people">
                 People
               </Link>
             </Menu.Item>
-            <Menu.Item key="2">
+            <Menu.Item key="planets">
               <Link to="/planets">
                 Planets
               </Link>
             </Menu.Item>
-            <Menu.Item key="3">
+            <Menu.Item key="films">
               <Link to="/films">
                 Films
               </Link>
             </Menu.Item>
-            <Menu.Item key="4">
+            <Menu.Item key="species">
               <Link to="/species">
                 Species
               </Link>
             </Menu.Item>
-            <Menu.Item key="5">
+            <Menu.Item key="vehicles">
               <Link to="/vehicles">
                 Vehicles
               </Link>
             </Menu.Item>
-            <Menu.Item key="6">
+            <Menu.Item key="starships">
               <Link to="/starships">
                 Starships
               </Link>
